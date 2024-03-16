4b97eebe73ebea390f639aafad4e4217a5987a66
fatma <fatmamahrous1510@gmail.com>
2024-03-16 16:58:58 +0200

    final cart code

--- OnlineShoppingApp/wwwroot/js/cart.js
+++ OnlineShoppingApp/wwwroot/js/cart.js
@@ -1,26 +1,60 @@
-﻿
+﻿// Get all radio buttons with name "news"
+var radioButtons = document.querySelectorAll('input[name="news"]');
 
-$(document).ready(function () {
-    $(".btn-number").click(function (e) {
-        e.preventDefault();
-        console.log("button click");
-        var fieldName = $(this).attr('data-field');
-        var type = $(this).attr('data-type');
-        var input = $("input[name='quant[" + fieldName + "]']");
-        var oldValue = parseFloat(input.val());
-        var newVal;
+// Add event listener to each radio button
+   radioButtons.forEach(function (radio) {
+        radio.addEventListener('change', function () {
+            // Check if the radio button is checked
+            if (this.checked) {
+                // Get the delivery cost associated with the selected shipping option
+                var deliveryCost = this.dataset.deliveryCost;
 
-        if (type === 'minus') {
-            newVal = oldValue - 1;
-        } else {
-            newVal = oldValue + 1;
-        }
+                // Update the selected shipping option in the <li> element
+                document.getElementById("selected-shipping").innerText = '$ ' + deliveryCost;
 
-        if (newVal <= 0) {
-            newVal = 1;
-        }
-
-        input.val(newVal);
-        // You might want to update the quantity on the server side here.
+                // Update the "You Pay" total
+                updateTotal(deliveryCost);
+            }
+        });
     });
-});
+
+
+// Function to update the "You Pay" total
+function updateTotal(deliveryCost) {
+    var cartTotal = parseFloat(document.querySelector('.cart-total-amount span').innerText.replace("$",""));
+
+   // var cartTotal = this.dataset.cartTotal;
+    console.log(cartTotal);
+   
+
+    // Calculate the total
+    var total = cartTotal + parseFloat(deliveryCost);
+
+    // Update the "You Pay" total
+    document.querySelector('.last span').innerText = '$' + total.toFixed(2);
+}
+
+
+  
+function checkout(count) {
+    // Get the selected delivery method
+    var selectedDeliveryMethod = document.querySelector('input[name="news"]:checked');
+
+    // If a delivery method is selected, proceed to checkout
+    if (count == 0) 
+    {
+          alert('Your cart is empty');
+
+    } 
+    else if (!selectedDeliveryMethod)
+    {
+        alert('Please choose a delivery method before proceeding to checkout.');
+
+    }
+    else {
+       
+        window.location.href = '/Home/index';
+
+    }
+}
+
